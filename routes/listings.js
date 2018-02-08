7922f331d05489eef431e236eed6048d63fafb95
Daniel Carapia <toonlink4943@gmail.com>
2018-02-08 08:49:36 -0700

    listings.json added, has the right format for what is needed on listings page Moved require line from index.js to listings.js

--- routes/listings.js
+++ routes/listings.js
@@ -1,6 +1,12 @@
+var fs = require('file-system');
+
 exports.index = function(req, res)
 {
+    var content = fs.readFileSync("listings.json");
+    var jsonContent = JSON.parse(content);
+    
     res.render('listings', {
         "title": "Listings",
+        category1: jsonContent.category1
     });
 }
